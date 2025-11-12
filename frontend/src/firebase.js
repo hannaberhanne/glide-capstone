730badce2803a2364e43fe2066f2eda9c0ee7778
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2025-11-12 11:07:02 -0500

    vite updates and env sut

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,17 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+require('dotenv').config();
+
 
 // Pull from  .env file
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: "glide-a52df",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
-console.log("Loaded Firebase key:", import.meta.env.VITE_FIREBASE_API_KEY);
+console.log("Loaded Firebase key:", process.env.FIREBASE_API_KEY);
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
