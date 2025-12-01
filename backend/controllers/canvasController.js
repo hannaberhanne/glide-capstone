e8d5e2a0ee3e46d49b5a4a09f58690bf7acebf3a
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2025-11-30 20:26:51 -0500

    canvas service for sync

---
+++ backend/controllers/canvasController.js
@@ -0,0 +1,8 @@
+import CanvasService from '../services/canvasService.js';
+import { db, admin } from '../config/firebase.js';
+
+// sync canvas to get both courses and assignments fom canvas to db
+const syncCanvas = async (req, res) => {
+    
+
+}
