f67a71736d4aa7bf52dd27f952317df9f4e097c1
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2026-04-23 15:19:07 -0400

    update on port

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 
 // Route imports
 import aiRoutes from './routes/aiRoutes.js';
