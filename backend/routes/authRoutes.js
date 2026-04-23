637221b65b073a179d9f5bf8792490f7ae409ea5
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2026-04-23 18:53:24 -0400

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,10 @@
 import express from 'express';
+import verifyToken from '../middleware/authMiddleware.js';
 import { signUp, login } from '../controllers/authController.js';
 
 const router = express.Router();
 
-
-router.post('/signup', signUp);  // this means post request for creating a new user at '/api/auth/signup'
-
-router.post('/login', login)  // /api/auth/login
+router.post('/signup', verifyToken, signUp);
+router.post('/login', verifyToken, login);
 
 export default router;
